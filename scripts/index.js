253bbb625770b822f4f2bec06a6a3cbafabd464f
00000dhrubojyoti <00000dhrubojyoti@gmail.com>
2025-01-25 17:24:19 +0000

    add testimonial

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Image Section
 const backBtn = document.querySelector('#back-btn')
 const nextBtn = document.querySelector('#next-btn')
 const images = document.querySelectorAll('.image')
@@ -25,3 +26,27 @@ nextBtn.addEventListener('click', () => {
         })
     }
 })
+
+// Testimonials Seciton
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 48,
+      },
+    },
+  });
+  
+  
